3e99464af7b8fa6cd702c885b49c7ffa6bde9dbc
jcbhmr <jcbhmr@outlook.com>
2023-11-27 13:51:38 -0600

    try this

--- implementation.js
+++ implementation.js
@@ -6,53 +6,45 @@
  * @param {number} [height]
  * @returns {void}
  */
-module.exports = (url, width = undefined, height = undefined) =>
-  void (async () => {
+module.exports = (url, width = 320, height = 180) => {
+  (async () => {
     url = `${url}`;
-    /** @type {HTMLImageElement | undefined} */
-    let image;
-    if (typeof Image !== "undefined") {
-      image = await new Promise((resolve, reject) => {
-        const image = new Image();
-        image.crossOrigin = "";
-        image.src = url;
-        image.onload = (event) => resolve(event.target);
-        image.onerror = () => resolve();
-      });
-    }
-
-    if (image) {
-      if (width !== undefined) {
-        image.width = width;
-      }
-      if (height !== undefined) {
-        image.height = height;
-      }
-    }
+    width = +width
+    height = +height
 
     /** @type {string | undefined} */
     let dataURL;
-    if (image && typeof document !== "undefined") {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext("2d");
-      canvas.width = image.width;
-      canvas.height = image.height;
-      ctx.drawImage(image, 0, 0);
-      dataURL = canvas.toDataURL();
+    if (url.startsWith("data:")) {
+      dataURL = url
+    } else if (typeof FileReader !== "undefined") {
+      const response = await fetch(url).catch(() => {})
+      if (response) {
+        const blob = await response.blob()
+        dataURL = await new Promise((resolve, reject) => {
+          const fr = new FileReader()
+          fr.onload = (event) => resolve(event.target.result)
+          fr.onerror = () => resolve()
+          fr.readAsDataURL(blob)          
+        })
+        console.log(dataURL?.slice(0, 100))
+      }
     }
 
-    console.log(
-      "%c%s",
-      `color: blue;
-    text-decoration: underline;
-    ${
-      dataURL
-        ? `background: url("${dataURL}");
-    font-size: 0;
-    line-height: ${image.height}px;
-    padding-right: ${image.width}px`
-        : ""
-    }`,
-      url,
-    );
+    if (width && height) {
+      console.log(
+        "%c%s",
+        `color: blue;
+        text-decoration: underline;
+        ${
+          dataURL
+            ? `background: url("${dataURL}") left top no-repeat;
+              font-size: 0;
+              line-height: ${height}px;
+              padding-right: ${width}px`
+            : ""
+        }`,
+        url,
+      );
+    }
   })();
+}
